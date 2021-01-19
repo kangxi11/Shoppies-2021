749a50e0235e5215bf89088680f14c813ea3a86c
Ryan Kang <rkang2009@gmail.com>
2021-01-19 12:46:14 -0700

    basic styling of search bar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,15 +1,42 @@
 import React from 'react';
 
-const searchBar = (props) => (
-    <div>
-        <input
-            type="text"
-            value={props.searchValue}
-            style={{width: '300px'}}
-            onChange={props.changed}
-        />
-        <button onClick={props.searchClicked}>Search</button>
-    </div>
-);
+import styles from './SearchBar.module.css';
+import searchIcon from '../../assets/search.png';
+
+import IconButton from "@material-ui/core/IconButton";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import SearchIcon from "@material-ui/icons/Search";
+import TextField from '@material-ui/core/TextField';
+
+const searchBar = (props) => {
+
+    const keyDownHandler = (e) => {
+        if (e.key === 'Enter') {
+            return props.searchClicked();
+        }
+    }
+
+    return (
+        <div>
+            <TextField
+                className={styles.Search}
+                InputProps={{
+                    startAdornment: (
+                    <InputAdornment onClick={props.searchClicked}>
+                        <IconButton>
+                        <SearchIcon />
+                        </IconButton>
+                    </InputAdornment>
+                    )
+                }}
+                placeholder="Search movies"
+                style={{backgroundImage: `url("${searchIcon}")`}}
+                value={props.searchValue}
+                onChange={props.changed}
+                onKeyDown={keyDownHandler}
+            />
+        </div>
+    );
+};
 
 export default searchBar;
