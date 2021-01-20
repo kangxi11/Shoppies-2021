8e5362226d20b1efd6ab3b75cb49b0f9e2dd89b0
Ryan Kang <rkang2009@gmail.com>
2021-01-19 22:01:13 -0700

    added animation to nomination list

--- src/components/Nominations/Nomination/Nomination.js
+++ src/components/Nominations/Nomination/Nomination.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import styles from './Nomination.module.css';
 
-const nomination = (props) => (
-    <div className={styles.Nomination}>
-        <p className={styles.Title}>{props.title}</p>
-        <p className={styles.Year}>{props.year}</p>
-        <div
-            onClick={props.nominationRemoved}
-            className={styles.close}
-        />
-    </div>
-);
+class Nomination extends Component {
+    state = {
+        container: [styles.Container, styles.Right, styles.Nomination]
+    }
 
-export default nomination;
+    componentDidMount() {
+        setTimeout(this.dropdown, 10);
+    }
+
+    dropdown = () => {
+        this.setState({container: [styles.Container, styles.Left, styles.Nomination]});
+    }
+
+    render() {
+        return (
+            <div className={styles.Wrapper}>
+                <div className={this.state.container.join(' ')}>
+                    <p className={styles.Title}>{this.props.title}</p>
+                    <p className={styles.Year}>{this.props.year}</p>
+                    <div
+                        onClick={this.props.nominationRemoved}
+                        className={styles.close}
+                    />
+                </div>    
+            </div>
+        );
+    }
+}
+
+export default Nomination;
